f2f446e3220549970fb32f022bc5de899e9267f2
Konstantin Ivanin <ivanin.k@gmail.com>
2025-12-09 15:10:33 +0300

    vlang raindrops
    Signed-off-by: Konstantin Ivanin <ivanin.k@gmail.com>

---
+++ vlang/raindrops/raindrops.v
@@ -0,0 +1,20 @@
+module main
+
+fn raindrops(number int) string {
+	mut res := ''
+
+	if number % 3 == 0 {
+		res += 'Pling'
+	}
+	if number % 5 == 0 {
+		res += 'Plang'
+	}
+	if number % 7 == 0 {
+		res += 'Plong'
+	}
+	if res == '' {
+		res = number.str()
+	}
+
+	return res
+}
