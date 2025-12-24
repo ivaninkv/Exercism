cc11c082d19b73dc2506c5b72664fa94022b7333
Konstantin Ivanin <ivanin.k@gmail.com>
2025-12-24 10:53:49 +0300

    zig raindrops improve
    Signed-off-by: Konstantin Ivanin <ivanin.k@gmail.com>

--- zig/raindrops/raindrops.zig
+++ zig/raindrops/raindrops.zig
@@ -2,25 +2,11 @@ const std = @import("std");
 
 pub fn convert(buffer: []u8, n: u32) []const u8 {
     var stream = std.io.fixedBufferStream(buffer);
-    const writer = stream.writer();
-    var wrote = false;
 
-    if (n % 3 == 0) {
-        writer.writeAll("Pling") catch unreachable;
-        wrote = true;
-    }
-    if (n % 5 == 0) {
-        writer.writeAll("Plang") catch unreachable;
-        wrote = true;
-    }
-    if (n % 7 == 0) {
-        writer.writeAll("Plong") catch unreachable;
-        wrote = true;
-    }
-
-    if (!wrote) {
-        writer.print("{d}", .{n}) catch unreachable;
-    }
+    if (n % 3 == 0) stream.writer().writeAll("Pling") catch unreachable;
+    if (n % 5 == 0) stream.writer().writeAll("Plang") catch unreachable;
+    if (n % 7 == 0) stream.writer().writeAll("Plong") catch unreachable;
+    if (stream.pos == 0) stream.writer().print("{d}", .{n}) catch unreachable;
 
     return stream.getWritten();
 }
