a66bcb07d645c426983795a9f0ccc755a7b1701e
Konstantin Ivanin <ivanin.k@gmail.com>
2026-01-12 17:16:27 +0300

    go difference-of-squares
    Signed-off-by: Konstantin Ivanin <ivanin.k@gmail.com>

---
+++ go/difference-of-squares/difference_of_squares.go
@@ -0,0 +1,21 @@
+package diffsquares
+
+func SquareOfSum(n int) int {
+	res := 0
+	for i := 1; i <= n; i++ {
+		res += i
+	}
+	return res * res
+}
+
+func SumOfSquares(n int) int {
+	sum := 0
+	for i := 1; i <= n; i++ {
+		sum += i * i
+	}
+	return sum
+}
+
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
